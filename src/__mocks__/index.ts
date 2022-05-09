7f5c3aefd2a60717b4a310e88dcfbe671bbe9a45
PauloMarquess <paulinho_marques2012@hotmail.com>
2022-05-09 19:08:39 -0300

    mocks para simular api

--- src/__mocks__/index.ts
+++ src/__mocks__/index.ts
@@ -18,3 +18,18 @@ export const RegisterMock = [
     nameInput: 'Nome',
   },
 ];
+
+export const UserMocks = [
+  {
+    id: 1,
+    user: 'User 1',
+  },
+  {
+    id: 2,
+    user: 'User 2',
+  },
+  {
+    id: 3,
+    user: 'User 3',
+  },
+];
